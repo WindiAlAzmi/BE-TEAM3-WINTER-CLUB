340fc336a3c26367cf0ed51e0d9ea99bc66cd367
Ratna Amalia Azzahra <amaliaratna41@gmail.com>
2023-12-22 19:05:29 +0700

    update seed.js db.js dan index.js

--- seed/seed.js
+++ seed/seed.js
@@ -9,21 +9,21 @@ async function seedData() {
     //fill data
     await db.collection("user").insertMany([
       {
-        id: 1,
+        id: 4,
         name: "nadia",
         role: "owner",
         behaviour: "ramah",
         status_adopter: false,
       },
       {
-        id: 2,
+        id: 5,
         name: "ratna",
         role: "adopter",
         behaviour: "tidak ramah",
         status_adopter: false,
       },
       {
-        id: 3,
+        id: 6,
         name: "windi",
         role: "adopter",
         behaviour: "ramah",
@@ -32,19 +32,19 @@ async function seedData() {
     ]);
     await db.collection("animal").insertMany([
       {
-        id: 1,
+        id: 4,
         name: "mocca",
         category: "cat",
         status_adoption: false,
       },
       {
-        id: 2,
+        id: 5,
         name: "kiwi",
         category: "rabbit",
         status_adoption: true,
       },
       {
-        id: 3,
+        id: 6,
         name: "redOne",
         category: "cat",
         status_adoption: false,
@@ -52,7 +52,7 @@ async function seedData() {
     ]);
      await db.collection("adopter").insertMany([
        {
-         id: 1,
+         id: 3,
          user: {
            $id: 3,
            $name: "windi",
